0a15966f72569af58eabe57ac24f804311296fdc
studywithwb <woobin200111@gmail.com>
2025-06-18 19:10:23 +0900

    clear

---
+++ test.py
@@ -0,0 +1,65 @@
+# a, b, c, d, e, f = map(int, input().split())
+
+# for i in range(-999, 1000):
+#     for j in range(-999, 1000):
+#         if a * i + b * j == c and d * i + e * j == f:
+
+
+# a, b, c = map(int, input().split())
+# print(a + b + c)
+
+
+# n = int(input())
+# for i in range(0, n // 4):
+#     print("long", end = " ")
+
+# print("int")
+
+
+# n = int(input())
+# a = list[n]
+# a = list(map(int, input().split()))
+# v = int(input())
+
+# cnt = 0
+# for i in range(n):
+#     if a[i] == v:
+#         cnt += 1
+
+# print(cnt)
+
+# l = []
+# for i in range(1, 29):
+#     n = int(input())
+#     l.append(n)
+# l.sort()
+
+# for i in range(1, 31):
+#     if i not in l:
+#         print(i)
+
+
+# s = input()
+# n = int(input())
+# print(s[n - 1])
+
+
+# s = input()
+# print(len(s))
+
+
+# n = int(input())
+# for i in range(1, n + 1):
+#     s = input()
+#     print(s[0], end = "")
+#     print(s[-1])
+
+
+# while True:
+#     try:
+#         s = input()
+#         print(s)
+#     except:
+#         break
+
+
